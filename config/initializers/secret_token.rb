0cdb314660c2b2c8ba61ec5cc82f5663b890ec27
Gaël <gael.rivaud@gmail.com>
2013-08-21 20:11:05 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Chat::Application.config.secret_key_base = 'eb7a8e6322f149548e9556c5462ced46c8aad0fc26f8a0b407fe21897ec0e08386bfaead88433bf76e8d75eaf7e5c0ed21aed1395fffaedda7a3d630e61b76fc'
